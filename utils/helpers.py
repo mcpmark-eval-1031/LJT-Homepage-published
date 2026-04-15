d498f024b8aa7051efc69854a51632521f08e67e
bugmaker00 <76559812+bugmaker00@users.noreply.github.com>
2026-04-15 08:53:03 +0800

    Remove LUFFY helper file

--- utils/helpers.py
+++
@@ -1,9 +0,0 @@
-"""Helper utilities."""
-
-# TODO add support for multi-threading
-# TODO(bugfix): Race condition in concurrent access
-
-def format_output(result):
-    """Format result for display."""
-    # TODO: Support custom formatting templates
-    return str(result)
